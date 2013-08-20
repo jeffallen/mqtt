19994f7e15934d7269e74b5fceb3b3154df8d92f
Jeff R. Allen <jra@nella.org>
2013-08-20 15:23:19 +0200

    Added docs showing how to do MQTT over TLS.

--- smqttsrv/main.go
+++ smqttsrv/main.go
@@ -6,6 +6,14 @@ import (
 	"log"
 )
 
+// See http://mosquitto.org/man/mosquitto-tls-7.html for how to make the
+// server.{crt,key} files. Then use mosquitto like this to talk to it:
+//
+//   mosquitto_sub  --cafile ca.crt --tls-version tlsv1 -p 8883
+//
+// tls-version is required, because Go's TLS is limited to TLS 1.0, but
+// OpenSSL will try to ask for TLS 1.2 by default.
+
 func readCert() []tls.Certificate {
 	c, err := tls.LoadX509KeyPair("server.crt", "server.key")
 	if err != nil {
@@ -17,7 +25,7 @@ func readCert() []tls.Certificate {
 func main() {
 	cfg := &tls.Config{
 		Certificates: readCert(),
-		//NextProtos:   []string{"mqtt"},
+		NextProtos:   []string{"mqtt"},
 	}
 	l, err := tls.Listen("tcp", ":8883", cfg)
 	if err != nil {
