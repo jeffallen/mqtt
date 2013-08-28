d49a7afc66c00f9b254ddb91d26683f3d0b2f70e
Jeff R. Allen <jra@nella.org>
2013-08-28 08:58:42 +0200

    Prepare for mega-conection testing.

--- mqttsrv/main.go
+++ mqttsrv/main.go
@@ -6,6 +6,52 @@ import (
 	"net"
 )
 
+type mlRes struct {
+	conn net.Conn
+	err error
+}
+
+type multiListener struct {
+	listeners []net.Listener
+	next chan mlRes
+}
+
+func newMultiListener(l ...net.Listener) *multiListener {
+	ml := &multiListener{
+		listeners: make([]net.Listener, len(l)),
+		next: make(chan mlRes, len(l)),
+	}
+	copy(ml.listeners, l)
+
+	for _,l := range ml.listeners {
+		go func(l net.Listener, next chan mlRes) {
+			for {
+				res := mlRes{}
+				res.conn, res.err = l.Accept()
+				next <- res
+			}
+		}(l, ml.next)
+	}
+
+	return ml
+}
+
+func (ml *multiListener)Accept() (net.Conn, error) {
+	res := <-ml.next
+	return res.conn, res.err
+}
+
+func (ml *multiListener)Close() error {
+	for _,l := range ml.listeners {
+		l.Close()
+	}
+	return nil
+}
+
+func (ml *multiListener)Addr() net.Addr {
+	return ml.listeners[0].Addr()
+}
+
 func main() {
 	l, err := net.Listen("tcp", ":1883")
 	if err != nil {
